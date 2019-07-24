2afd2456737c9db38b80f23e6412c65422bf15c2
执笔 <805073259@qq.com>
2019-07-24 17:56:04 +0800

    时间使用 Date

--- fast-mybatis/src/main/java/zhibi/fast/mybatis/dto/BaseDomain.java
+++ fast-mybatis/src/main/java/zhibi/fast/mybatis/dto/BaseDomain.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author 执笔
@@ -24,15 +24,15 @@ public abstract class BaseDomain extends BasePage implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @KeySql(useGeneratedKeys = true)
-    private Long          id;
+    private Long id;
     /**
      * 操作人
      */
-    private Long          operationId;
+    private Long operationId;
     @AutoTime(insert = true, update = false)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime createTime;
+    private Date createTime;
     @AutoTime(insert = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime updateTime;
+    private Date updateTime;
 }
