20140f465c99bfd5be3e96e913562440ddc71f2c
执笔 <805073259@qq.com>
2019-10-17 11:14:59 +0800

    添加工具方法  判断是否移动端访问

--- fast-commons/src/main/java/zhibi/fast/commons/utils/RequestUtils.java
+++ fast-commons/src/main/java/zhibi/fast/commons/utils/RequestUtils.java
@@ -2,6 +2,7 @@ package zhibi.fast.commons.utils;
 
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -119,5 +120,39 @@ public class RequestUtils {
             }
         }
         return buffer.toString();
+    }
+
+
+    /**
+     * 判断是否是移动端
+     *
+     * @param request
+     * @return
+     */
+    public static Boolean isMobile(HttpServletRequest request) {
+        String[] mobileAgents = {"iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+                "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
+                "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
+                "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
+                "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem",
+                "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos",
+                "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
+                "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
+                "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
+                "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
+                "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
+                "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
+                "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-",
+                "Googlebot-Mobile"};
+        String ua = request.getHeader("user-agent");
+        if (StringUtils.isNotBlank(ua)) {
+            for (String mobileAgent : mobileAgents) {
+                if (ua.toLowerCase().contains(mobileAgent)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
